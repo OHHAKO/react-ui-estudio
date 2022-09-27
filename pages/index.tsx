2034ea269a554508687115d4ea139aea2d0fe1c6
OHHAKO <l36h7129@gmail.com>
2022-09-27 17:35:21 +0900

    refactor: 페이지 공통 footer, header 컴포넌트로 분리

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,11 @@
 import styles from "./index.module.scss";
-import manul from "../public/assets/pallas.png";
-import Head from "next/head";
-import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 export default function Home(): React.ReactElement {
   return (
     <>
-      <Head>
-        <title>Hako Frontend Boilerplate</title>
-        <link rel="icon" href="/favicon.svg" />
-      </Head>
       <div className={styles.root}>
-        <div className={styles.header}>
-          <div className={styles.title}>
-            {/* Hako */}
-            User interface estudio
-          </div>
-          <div>by Hako</div>
-          <div className={styles.imgWrapper}>
-            <Image src={manul} alt="picture" width={250} height={180} />
-          </div>
-        </div>
         <div className={styles.body}>
           <ul>
             <li>
@@ -45,19 +28,6 @@ export default function Home(): React.ReactElement {
             </li>
             <li>
               <Link href="/filter-class">filter(Class component)</Link>
-            </li>
-          </ul>
-        </div>
-        <div className={styles.footer}>
-          <ul>
-            <li>
-              <Link href="https://github.com/OHHAKO">Git</Link>
-            </li>
-            <li>
-              <Link href="https://medium.com/@khk0503">Medium</Link>
-            </li>
-            <li>
-              <Link href="https://instagram.com/kimhrkeon?igshid=YmMyMTA2M2Y=">Insta</Link>
             </li>
           </ul>
         </div>
